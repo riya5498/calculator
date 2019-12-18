5b258c0f56004088d53cf4400f3711ba30b57760
riya5498 <riyay8276@gmail.com>
2019-12-18 21:16:00 +0530

    added calculator

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		add(3,4);
+
+	}
+	
+	public static void add(int num1, int num2) {
+		
+		System.out.println("Addition Results : " + (num1 + num2));
+		
+	}
+
+}
